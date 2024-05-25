74d3871a85504577979e5f6ead6953bf607bfab4
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-05-25 21:21:24 +0200

    USBX stack size increased

--- USBX/App/app_usbx_device.h
+++ USBX/App/app_usbx_device.h
@@ -45,7 +45,7 @@ extern "C" {
 /* Exported constants --------------------------------------------------------*/
 #define USBX_DEVICE_MEMORY_STACK_SIZE       10*1024
 
-#define UX_DEVICE_APP_THREAD_STACK_SIZE   1024
+#define UX_DEVICE_APP_THREAD_STACK_SIZE   2*1024
 #define UX_DEVICE_APP_THREAD_PRIO         10
 
 /* USER CODE BEGIN EC */
